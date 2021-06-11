03ac1bde4a3967b80cd18e30b10ddb7d01ad6803
Glitch (glitch-hello-node) <none>
2021-06-11 01:45:47 +0000

    🤑👂 Checkpoint ./package.json:18205372/5093 ./server.js:18205372/1620 ./db.config.js:18205372/304 ./models/UrlModel.js:18205372/393

---
+++ models/UrlModel.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+const UrlSchema = new mongoose.Schema({
+  code: String,
+  shortUrl: String,
+  longUrl: String,
+  date: {
+    type: String,
+    default: Date.now
+  }
+})
+
+const Url = mongoose.model('Url', UrlSchema)
+
+export default Url;
